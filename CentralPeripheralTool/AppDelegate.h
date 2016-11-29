d35eea75350fed9912f11aa43156d5b55428bec5
LiuMingchuan <LiuMingchuan@RyomaMBP.local>
2016-11-29 15:44:40 +0900

    Initial Commit

---
+++ CentralPeripheralTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CentralPeripheralTool
+//
+//  Created by LiuMingchuan on 2016/11/29.
+//  Copyright © 2016年 LiuMingchuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CentralPeripheralTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CentralPeripheralTool
+//
+//  Created by LiuMingchuan on 2016/11/29.
+//  Copyright © 2016年 LiuMingchuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
